54964e3b0e93d3b6db6defa91649f35e6d20ca50
sjoleee <cent7425@gmail.com>
2022-09-10 18:33:46 +0900

    feat: tailwindCSS 세팅 #4

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-   <div>bemyidol 입니당</div>
-  )
-}
+  return <div className="bg-green-500">bemyidol 입니당</div>;
+};
 
-export default Home
+export default Home;
