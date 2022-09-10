323642c68b6e38dc27e706749c9af922af9ffe22
sjoleee <cent7425@gmail.com>
2022-09-10 18:52:04 +0900

    feat: tailwindCSS 세팅 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
+import Header from '../src/components/Header';
 
 const Home: NextPage = () => {
-  return <div className="bg-green-500">bemyidol 입니당</div>;
+  return (
+    <div className="bg-green-500">
+      bemyidol 입니당
+      <Header></Header>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h1 className="text-green-300">tailwind test</h1>
+    </div>
+  );
+}
+
+export default Header;
