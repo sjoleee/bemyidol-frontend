f4112e8e04d186204736912b15ba05bb27de9b49
sjoleee <cent7425@gmail.com>
2022-09-11 00:07:38 +0900

    fix: pages src 하위 폴더로 변경

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+
+import Header from '../components/Header';
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-green-500">
+      bemyidol 입니당
+      <Header></Header>
+    </div>
+  );
+};
+
+export default Home;
