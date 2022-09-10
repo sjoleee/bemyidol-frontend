5a07e680090d7622493c68e9516a23a0ea1e2675
sjoleee <cent7425@gmail.com>
2022-09-11 01:27:17 +0900

    fix: 컴포넌트를 표현식으로 수정, interface 이름 변경, export 줄바꿈 #2

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,9 @@
-interface AppLayoutProps {
+interface Props {
   children: React.ReactNode;
 }
 
-function Layout({ children }: AppLayoutProps) {
+const Layout = ({ children }: Props) => {
   return <div className="w-screen max-w-3xl m-auto">{children}</div>;
-}
+};
+
 export default Layout;
