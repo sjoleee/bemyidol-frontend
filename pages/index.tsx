d72b984015fe73a00a8bcbe3754f96096b3dc398
hy57in <gywls00100@gmail.com>
2022-09-10 23:54:06 +0900

    fix: prettier, 관련 plugin 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next';
+import type {NextPage} from 'next';
+
 import Header from '../src/components/Header';
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-green-500">
-      bemyidol 입니당
-      <Header></Header>
-    </div>
-  );
+    return (
+        <div className="bg-green-500">
+            bemyidol 입니당
+            <Header></Header>
+        </div>
+    );
 };
 
 export default Home;
