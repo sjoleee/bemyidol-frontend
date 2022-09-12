9451d2bc1d31d36d097436a9164d5c1bc8c98aff
hy57in <gywls00100@gmail.com>
2022-09-12 16:44:08 +0900

    chore: 주석 제거

--- postcss.config.js
+++ postcss.config.js
@@ -10,24 +10,3 @@ module.exports = {
         autoprefixer: {},
     }
 }
-
-// module.exports = {
-//     plugins: [
-//         'tailwindcss/nesting',
-//         'tailwindcss',
-//         'postcss-flexbugs-fixes',
-//         [
-//             'postcss-preset-env',
-//             {
-//                 autoprefixer: {
-//                     flexbox: 'no-2009'
-//                 },
-//                 stage: 3,
-//                 features: {
-//                     'custom-properties': false,
-//                     'nesting-rules': false
-//                 }
-//             }
-//         ]
-//     ]
-// };
