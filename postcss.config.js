374c0f2368095ea1226d44c60b9ec4e24209c03b
hy57in <gywls00100@gmail.com>
2022-09-12 16:43:00 +0900

    chore: postcss, tailwindcss config 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,33 @@
+// 설정변경하면 .next 디렉토리 지워야 제대로 반영됨.
+// https://nextjs.org/docs/advanced-features/customizing-postcss-config#customizing-plugins
+// https://tailwindcss.com/docs/using-with-preprocessors
+// https://github.com/csstools/postcss-preset-env/blob/main/src/lib/plugins-by-id.js#L36
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
+
+// module.exports = {
+//     plugins: [
+//         'tailwindcss/nesting',
+//         'tailwindcss',
+//         'postcss-flexbugs-fixes',
+//         [
+//             'postcss-preset-env',
+//             {
+//                 autoprefixer: {
+//                     flexbox: 'no-2009'
+//                 },
+//                 stage: 3,
+//                 features: {
+//                     'custom-properties': false,
+//                     'nesting-rules': false
+//                 }
+//             }
+//         ]
+//     ]
+// };
