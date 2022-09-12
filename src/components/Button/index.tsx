f471a2a9c69d940ac0e0c0df108191e326247f27
hy57in <gywls00100@gmail.com>
2022-09-12 16:42:44 +0900

    feat: Button 컴포넌트 필요없는 prop 정리

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,91 +1,50 @@
-import { ButtonHTMLAttributes, forwardRef } from 'react';
+import {ButtonHTMLAttributes, forwardRef} from 'react';
 import styles from './index.module.css';
 import clsx from 'clsx';
 
 export enum ButtonSize {
-  XS = 'x-small',
-  SM = 'small',
-  MD = 'medium',
-  LG = 'large',
-}
-
-export enum ButtonColor {
-  PRIMARY = 'primary',
-  SUB = 'sub',
-  MONO = 'mono',
-  WARNING = 'warning',
-  INFO = 'info',
-  /**
-   * fixed color : black/white 는 테마 변경에 반영되지 않음
-   */
-  BLACK = 'black',
-  /**
-   * fixed color : black/white 는 테마 변경에 반영되지 않음
-   */
-  WHITE = 'white',
-  ELEVATED = 'elevated',
-}
-
-export enum ButtonVariant {
-  CONTAINED = 'contained',
-  OUTLINED = 'outlined',
-  TEXT = 'text',
+    XS = 'x-small',
+    SM = 'small',
+    MD = 'medium',
+    LG = 'large',
 }
 
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-  size?: ButtonSize;
-  fullWidth?: boolean;
-  fullRounded?: boolean;
-  color?: ButtonColor;
-  variant?: ButtonVariant;
-  prefixIcon?: React.ReactNode;
-  suffixIcon?: React.ReactNode;
-  icon?: React.ReactNode;
+    size?: ButtonSize;
+    fullWidth?: boolean;
+    fullRounded?: boolean;
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(
-  (
-    {
-      children,
-      className,
-      size = ButtonSize.SM,
-      fullWidth = false,
-      fullRounded = false,
-      color = ButtonColor.PRIMARY,
-      variant = ButtonVariant.CONTAINED,
-      prefixIcon,
-      suffixIcon,
-      icon,
-      ...rest
+    (
+        {
+            children,
+            className,
+            size = ButtonSize.SM,
+            fullWidth = false,
+            fullRounded = false,
+            ...rest
+        },
+        ref,
+    ) => {
+        return (
+            <button
+                className={clsx(
+                    styles.container,
+                    styles[size],
+                    {
+                        [styles.fullWidth]: fullWidth,
+                        [styles.rounded]: fullRounded,
+                    },
+                    className,
+                )}
+                ref={ref}
+                {...rest}
+            >
+                {children}
+            </button>
+        );
     },
-    ref,
-  ) => {
-    return (
-      <button
-        className={clsx(
-          styles.container,
-          styles[size],
-          styles[variant],
-          styles[color],
-          {
-            [styles.fullWidth]: fullWidth,
-            [styles.withPrefixIcon]: prefixIcon,
-            [styles.withSuffixIcon]: suffixIcon,
-            [styles.iconOnly]: icon && !children,
-            [styles.rounded]: fullRounded,
-          },
-          className,
-        )}
-        ref={ref}
-        {...rest}
-      >
-        {prefixIcon && <div className={styles.prefixIcon}>{prefixIcon}</div>}
-        {icon}
-        {children}
-        {suffixIcon && <div className={styles.suffixIcon}>{suffixIcon}</div>}
-      </button>
-    );
-  },
 );
 
 export default Button;
