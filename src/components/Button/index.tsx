03241d76b9a188481a5e1f85f9c61af77926d37d
Sangjo LEE <cent7425@gmail.com>
2022-09-12 17:12:03 +0900

    feat: casting markup (#17)
    * fix: solve conflict
    
    * feat: casting페이지 마크업 #9

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,50 +1,43 @@
-import {ButtonHTMLAttributes, forwardRef} from 'react';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 import styles from './index.module.css';
 import clsx from 'clsx';
 
 export enum ButtonSize {
-    XS = 'x-small',
-    SM = 'small',
-    MD = 'medium',
-    LG = 'large',
+  XS = 'x-small',
+  SM = 'small',
+  MD = 'medium',
+  LG = 'large',
 }
 
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-    size?: ButtonSize;
-    fullWidth?: boolean;
-    fullRounded?: boolean;
+  size?: ButtonSize;
+  fullWidth?: boolean;
+  fullRounded?: boolean;
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(
-    (
-        {
-            children,
-            className,
-            size = ButtonSize.SM,
-            fullWidth = false,
-            fullRounded = false,
-            ...rest
-        },
-        ref,
-    ) => {
-        return (
-            <button
-                className={clsx(
-                    styles.container,
-                    styles[size],
-                    {
-                        [styles.fullWidth]: fullWidth,
-                        [styles.rounded]: fullRounded,
-                    },
-                    className,
-                )}
-                ref={ref}
-                {...rest}
-            >
-                {children}
-            </button>
-        );
-    },
+  (
+    { children, className, size = ButtonSize.SM, fullWidth = false, fullRounded = false, ...rest },
+    ref,
+  ) => {
+    return (
+      <button
+        className={clsx(
+          styles.container,
+          styles[size],
+          {
+            [styles.fullWidth]: fullWidth,
+            [styles.rounded]: fullRounded,
+          },
+          className,
+        )}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </button>
+    );
+  },
 );
 
 export default Button;
--- src/pages/casting.tsx
+++ src/pages/casting.tsx
@@ -1,15 +1,41 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
-import { H1 } from '@/components/Text';
+import Search from '@/components/Search/index';
+import Card from '@/components/Card/index';
+import Button from '@/components/Button';
+
+interface Props {
+  id: number;
+  groupName: string;
+  name: string;
+  squreImageUrl: string;
+  longImageUrl: string;
+  debutDate: string;
+}
 
 const Casting: NextPage = () => {
+  const [data, setData] = useState<Props[]>([]);
+
+  useEffect(() => {
+    fetch('/data/mockdata.json')
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center">
+    <div>
       <Head>
         <title>bemyidol</title>
       </Head>
-      <H1>아이돌 메이커</H1>
+      <Search></Search>
+      <div className="flex flex-wrap gap-3">
+        {data.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
+      </div>
+      <Button className="bg-green-500">다음</Button>
     </div>
   );
 };
