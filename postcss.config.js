b50aa3a8d4950a18fa5975f967f1dd500ce4f213
HyojinKim <gywls00100@gmail.com>
2022-09-12 17:02:02 +0900

    feat: Button 컴포넌트 추가 (#15)
    * feat: Button 컴포넌트 추가
    
    * chore: import/order warn으로 변경
    
    * fix: displayname 추가
    
    * feat: Button 컴포넌트 필요없는 prop 정리
    
    * chore: postcss, tailwindcss config 수정
    
    * chore: 주석 제거
    
    * fix: 충돌 잘못반영한 것 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+// 설정변경하면 .next 디렉토리 지워야 제대로 반영됨.
+// https://nextjs.org/docs/advanced-features/customizing-postcss-config#customizing-plugins
+// https://tailwindcss.com/docs/using-with-preprocessors
+// https://github.com/csstools/postcss-preset-env/blob/main/src/lib/plugins-by-id.js#L36
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
+        tailwindcss: {},
+        autoprefixer: {},
+    }
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,52 @@
-import style from '@/components/Button/index.module.css';
+import {ButtonHTMLAttributes, forwardRef} from 'react';
+import styles from './index.module.css';
+import clsx from 'clsx';
 
-interface Props {
-  children: React.ReactNode;
+export enum ButtonSize {
+    XS = 'x-small',
+    SM = 'small',
+    MD = 'medium',
+    LG = 'large',
 }
 
-const Button = ({ children }: Props) => {
-  return <button className={style.buttonprimary}>{children}</button>;
+export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+    size?: ButtonSize;
+    fullWidth?: boolean;
+    fullRounded?: boolean;
 };
 
+const Button = forwardRef<HTMLButtonElement, Props>(
+    (
+        {
+            children,
+            className,
+            size = ButtonSize.SM,
+            fullWidth = false,
+            fullRounded = false,
+            ...rest
+        },
+        ref,
+    ) => {
+        return (
+            <button
+                className={clsx(
+                    styles.container,
+                    styles[size],
+                    {
+                        [styles.fullWidth]: fullWidth,
+                        [styles.rounded]: fullRounded,
+                    },
+                    className,
+                )}
+                ref={ref}
+                {...rest}
+            >
+                {children}
+            </button>
+        );
+    },
+);
+
 export default Button;
+
+Button.displayName = 'Button';
