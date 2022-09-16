8894088e664c06359a0d0b81c9b8732a12832947
sjoleee <cent7425@gmail.com>
2022-09-17 01:34:15 +0900

    fix: 검색에 debounce 적용

--- src/components/MemberCardList/index.tsx
+++ src/components/MemberCardList/index.tsx
@@ -1,12 +1,15 @@
 import MemberCard from '../MemberCard';
 
 import { MemberStore, SearchTextStore } from '@/store/store';
+import useDebounce from '@/hooks/useDebounce';
 
 const MemberCardList = () => {
   const { members } = MemberStore();
   const { searchText } = SearchTextStore();
+  const debouncedSearchText = useDebounce(searchText, 200);
   const filteredMembers = [...members].filter(
-    (item) => item.name.includes(searchText) || item.groupName.includes(searchText),
+    (item) =>
+      item.name.includes(debouncedSearchText) || item.groupName.includes(debouncedSearchText),
   );
   return (
     <div className="flex flex-wrap gap-3">
