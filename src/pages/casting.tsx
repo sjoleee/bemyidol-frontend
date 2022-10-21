228a4d9cbce40821c0ef2a3a6e31f2d8ca292213
sjoleee <cent7425@gmail.com>
2022-10-21 18:54:03 +0900

    feat: Button 컴포넌트 스타일 수정, Search 페이지에서 Button disabled를 위한 로직 작성

--- src/pages/casting.tsx
+++ src/pages/casting.tsx
@@ -1,27 +1,41 @@
+import { useEffect, useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 
+import { H2, T2 } from '@/components/Text';
 import Search from '@/components/Search/index';
 import Button from '@/components/Button';
 import MemberCardList from '@/components/MemberCardList';
 import SelectedMemberCardList from '@/components/SelectedMemberCardList';
+import { SelectedMemberStore } from '@/store/store';
 
 const Casting: NextPage = () => {
+  const { selectedMembers } = SelectedMemberStore();
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (selectedMembers.length > 1 && selectedMembers.length < 11) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [selectedMembers]);
   return (
-    <div>
+    <div className="relative h-screen overflow-hidden p-4">
       <Head>
         <title>bemyidol</title>
       </Head>
+      <H2>멤버를 캐스팅해주세요 ⭐️</H2>
       <Search />
       <MemberCardList />
-      <SelectedMemberCardList />
+      {selectedMembers.length ? <SelectedMemberCardList /> : null}
       <Link href="/setting">
-        <a>
-          <Button fullWidth={true} isFixed={true}>
-            다음
-          </Button>
-        </a>
+        <Button isFixed={true} disabled={isDisabled}>
+          <T2 className="text-white">
+            {isDisabled ? '최소 2명 ~ 최대 10명을 선택해주세요' : '캐스팅 완료 👌'}
+          </T2>
+        </Button>
       </Link>
     </div>
   );
