3c260732931976687c62e400d34e18e61a8e8a9a
sjoleee <cent7425@gmail.com>
2022-10-21 19:00:44 +0900

    feat: Text 스타일 수정 및 className과 styles.module.css를 동시 적용할 수 있도록 clsx 사용, font-family 적용

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,29 +1,36 @@
-import style from '@/components/Text/index.module.css';
+import clsx from 'clsx';
+
+import styles from '@/components/Text/index.module.css';
 
 interface Props {
+  className?: string;
   children: React.ReactNode;
 }
 
-export const H1 = ({ children }: Props) => {
-  return <h1 className={style.h1}>{children}</h1>;
+export const H1 = ({ children, className }: Props) => {
+  return <h1 className={clsx(styles.h1, [className])}>{children}</h1>;
 };
 
-export const H2 = ({ children }: Props) => {
-  return <h2 className={style.h2}>{children}</h2>;
+export const H2 = ({ children, className }: Props) => {
+  return <h2 className={clsx(styles.h2, [className])}>{children}</h2>;
 };
 
-export const T1 = ({ children }: Props) => {
-  return <span className={style.t1}>{children}</span>;
+export const T1 = ({ children, className }: Props) => {
+  return <span className={clsx(styles.t1, [className])}>{children}</span>;
 };
 
-export const T2 = ({ children }: Props) => {
-  return <span className={style.t2}>{children}</span>;
+export const T2 = ({ children, className }: Props) => {
+  return <span className={clsx(styles.t2, [className])}>{children}</span>;
 };
 
-export const T3 = ({ children }: Props) => {
-  return <span className={style.t3}>{children}</span>;
+export const T3 = ({ children, className }: Props) => {
+  return <span className={clsx(styles.t3, [className])}>{children}</span>;
 };
 
-export const T4 = ({ children }: Props) => {
-  return <span className={style.t4}>{children}</span>;
+export const T4 = ({ children, className }: Props) => {
+  return <span className={clsx(styles.t4, [className])}>{children}</span>;
+};
+
+export const T5 = ({ children, className }: Props) => {
+  return <span className={clsx(styles.t5, [className])}>{children}</span>;
 };
