bcf926ad4a452eb310e7ded1fa37e90c53f9cfda
sjoleee <cent7425@gmail.com>
2022-10-24 04:00:32 +0900

    feat: 컴포넌트 텍스트 사이즈 수정 및 불필요한 파일 삭제

--- src/pages/casting.tsx
+++ src/pages/casting.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 
-import { H2, T2 } from '@/components/Text';
+import { T1, T2 } from '@/components/Text';
 import Search from '@/components/Search/index';
 import Button from '@/components/Button';
 import MemberCardList from '@/components/MemberCardList';
@@ -26,7 +26,7 @@ const Casting: NextPage = () => {
       <Head>
         <title>bemyidol</title>
       </Head>
-      <H2>멤버를 캐스팅해주세요 ⭐️</H2>
+      <T1>멤버를 캐스팅해주세요 ⭐️</T1>
       <Search />
       <MemberCardList />
       {selectedMembers.length ? <SelectedMemberCardList /> : null}
