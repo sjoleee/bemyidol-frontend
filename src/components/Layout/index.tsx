d6120ada8b22ed0861f46770bcac4b2e6e3b6f95
sjoleee <cent7425@gmail.com>
2022-10-24 12:17:09 +0900

    feat: scrollbar 관련 플러그인 삭제, global.css에 전역으로 스크롤바 제거

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
-  return <div className="relative w-screen h-screen m-auto">{children}</div>;
+  return <div className="relative w-screen h-screen m-auto scrollbar-hide">{children}</div>;
 };
 
 export default Layout;
--- src/components/SelectedMemberCardList/index.tsx
+++ src/components/SelectedMemberCardList/index.tsx
@@ -6,7 +6,7 @@ const SelectedMemberCardList = () => {
   const { selectedMembers } = SelectedMemberStore();
   return (
     <div className="fixed flex p-4 bottom-0 w-full h-[134px] md:h-[170px] left-0 bg-GREY_LIGHT rounded-t-lg">
-      <div className="flex gap-2 w-full overflow-x-scroll scrollbar-none">
+      <div className="flex gap-2 w-full overflow-x-scroll">
         {selectedMembers.map((item) => (
           <SelectedMemberCard key={item.memberId} {...item} />
         ))}
