a64f500efb69e133f83efc806365974b9b7f7879
sjoleee <cent7425@gmail.com>
2022-12-01 14:53:01 +0900

    feat: main의 text strong처리, framer motion 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 import Button from '@/components/Button';
 import { H1, T2 } from '@/components/Text';
@@ -20,16 +21,22 @@ const Home: NextPage = () => {
   return (
     <div className="flex relative flex-col items-center h-full overflow-hidden">
       <Seo title="Home" />
-      <img className="w-32 md:w-40 mt-[30%] md:mt-[10%]" src="images/logo.png" alt="로고" />
-      <H1>BeMyIdol</H1>
-      <T2 className="text-center my-6">
+      <motion.div
+        className="flex flex-col items-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <img className="w-32 md:w-40 mt-[18vh] " src="images/logo.png" alt="로고" />
+        <H1>BeMyIdol</H1>
+      </motion.div>
+      <T2 className="text-center mt-12 ">
         꾸준한 덕질의 결실로
         <br />
-        ‘마스터 엔터테인먼트’의 CEO가 된 당신은
+        <strong>‘마스터 엔터테인먼트’</strong>의 CEO가 된 당신은
         <br />
         현존하는 최고의 아이돌 멤버들을 영입하여
         <br />
-        아이돌 어벤져스를 만들 계획이다.
+        <strong>아이돌 어벤져스</strong>를 만들 계획이다.
       </T2>
       <Link href="/casting">
         <Button isWhite={true} isFixed={true}>
