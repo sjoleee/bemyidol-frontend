91b693111806dd65bce190f06203294b54d5840f
sjoleee <cent7425@gmail.com>
2023-01-23 05:32:59 +0900

    feat: complete first section of main interaction

---
+++ src/components/MainFirstSection/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import MainFirstSectionTimer from "../MainFirstSectionTimer";
+
+import useObserver from "hooks/useObserver";
+import { INTRODUCE_TEXT_ARR } from "@/constants";
+
+const MainFirstSection = () => {
+  const [isFirstSectionSeen, setIsFirstSectionSeen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onIntersectFirstSection: IntersectionObserverCallback = ([entry]) => {
+    entry.isIntersecting ? setIsFirstSectionSeen(true) : setIsFirstSectionSeen(false);
+  };
+
+  const { setTarget: setTargetFirstSection } = useObserver({
+    onIntersect: onIntersectFirstSection,
+    threshold: 1,
+  });
+
+  return (
+    <section className="h-[430vh] w-full relative">
+      <div
+        className="h-screen w-full sticky top-0 left-0 flex flex-col justify-center items-center z-10"
+        ref={setTargetFirstSection}
+        style={{ opacity: isFirstSectionSeen ? 1 : 0, transition: "opacity 0.2s" }}
+      >
+        {INTRODUCE_TEXT_ARR.map((item, index) => {
+          return (
+            <>
+              {item.imgSrc && (
+                <img
+                  alt={item.content}
+                  src={item.imgSrc}
+                  className="object-cover w-screen h-screen opacity-20 absolute md:object-top blur-sm"
+                  style={{ opacity: currentIndex === index ? 0.2 : 0, transition: "opacity 0.3s" }}
+                />
+              )}
+              <div
+                key={item.id}
+                className="bg-PRIMARY px-4 py-2 mb-2 mx-2 rounded-lg absolute bottom-[50%]"
+                style={{ opacity: currentIndex === index ? 1 : 0, transition: "opacity 0.3s" }}
+              >
+                <span className="text-5xl font-bold text-white">{item.content}</span>
+              </div>
+            </>
+          );
+        })}
+        <span className="text-5xl font-bold absolute top-[50%]">좋아하세요?</span>
+      </div>
+      <div>
+        {INTRODUCE_TEXT_ARR.map((item, index) => {
+          return (
+            <MainFirstSectionTimer key={item.id} index={index} setCurrentIndex={setCurrentIndex} />
+          );
+        })}
+      </div>
+
+      {/* */}
+    </section>
+  );
+};
+
+export default MainFirstSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,100 @@
-import { useEffect } from 'react';
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import Link from "next/link";
+import Lottie from "react-lottie-player";
 
-import Button from '@/components/Button';
-import { H1, T2 } from '@/components/Text';
-import { DebutGroupStore, SelectedMemberStore } from '@/store/store';
-import Seo from '@/components/Seo';
+import lottieJson from "public/lotties/scrollDown.json";
+import Button from "@/components/Button";
+import { H1, T1, T2 } from "@/components/Text";
+import { DebutGroupStore, SelectedMemberStore } from "@/store/store";
+import Seo from "@/components/Seo";
+import useObserver from "hooks/useObserver";
+import MainFirstSection from "@/components/MainFirstSection";
 
 const Home: NextPage = () => {
   const { setSelectedMembers } = SelectedMemberStore();
   const { setDebutGroup } = DebutGroupStore();
+  const [isMainSeen, setIsMainSeen] = useState(true);
+  const [isSecondSectionSeen, setIsSecondSectionSeen] = useState(false);
 
   useEffect(() => {
-    localStorage.removeItem('scrollY');
+    localStorage.removeItem("scrollY");
     setSelectedMembers([]);
-    setDebutGroup([], '', '');
+    setDebutGroup([], "", "");
   }, []);
 
+  //메인 화면의 io
+  const onIntersectMain: IntersectionObserverCallback = ([entry]) => {
+    entry.isIntersecting ? setIsMainSeen(true) : setIsMainSeen(false);
+  };
+
+  const { setTarget: setTargetMain } = useObserver({
+    onIntersect: onIntersectMain,
+    threshold: 0,
+  });
+
+  //2번째 섹션의 io
+  const onIntersectSecondSection: IntersectionObserverCallback = ([entry]) => {
+    entry.isIntersecting ? setIsSecondSectionSeen(true) : setIsSecondSectionSeen(false);
+  };
+
+  const { setTarget: setTargetSecondSection } = useObserver({
+    onIntersect: onIntersectSecondSection,
+    threshold: 0.9,
+  });
+
   return (
-    <div className="flex relative flex-col items-center h-full overflow-hidden">
+    <div className="flex relative flex-col items-center">
       <Seo title="Home" />
-      <motion.div
-        className="flex flex-col items-center"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <img className="w-32 md:w-40 mt-[18vh] " src="images/logo.png" alt="로고" />
-        <H1>BeMyIdol</H1>
-      </motion.div>
-      <T2 className="text-center mt-12 ">
-        꾸준한 덕질의 결실로
-        <br />
-        <strong>‘마스터 엔터테인먼트’</strong>의 CEO가 된 당신은
-        <br />
-        현존하는 최고의 아이돌 멤버들을 영입하여
-        <br />
-        <strong>아이돌 어벤져스</strong>를 만들 계획이다.
-      </T2>
-      <Link href="/casting">
-        <Button isWhite={true} isFixed={true}>
-          <T2>걸그룹 만들러가기 🎀</T2>
-        </Button>
-      </Link>
-      <div className="absolute bg-PRIMARY -z-10 rounded-[180%] md:rounded-[120%] w-[180%] h-full -bottom-[55%] md:-bottom-1/2" />
+      <section className="h-screen">
+        <div className="flex flex-col items-center  relative h-full">
+          <img
+            className="w-32 md:w-40 mt-[18vh] "
+            src="images/logo.png"
+            alt="로고"
+            ref={setTargetMain}
+          />
+          <H1>BeMyIdol</H1>
+          <T1 className="text-center">나만의 아이돌을 만들고 공유해보세요!</T1>
+          <Lottie
+            className="absolute bottom-[10%]"
+            loop
+            animationData={lottieJson}
+            play
+            style={{
+              width: 50,
+              height: 50,
+              opacity: isMainSeen ? 1 : 0,
+              transition: "opacity 0.3s",
+            }}
+          />
+        </div>
+      </section>
+
+      <MainFirstSection />
+
+      <section className="h-[200vh] w-full relative">
+        <div
+          className="h-screen w-full bg-PRIMARY sticky top-0 left-0 flex flex-col justify-center items-center"
+          ref={setTargetSecondSection}
+          style={{ opacity: isSecondSectionSeen ? 1 : 0, transition: "opacity 0.3s" }}
+        >
+          <T1 className="w-full h-[100px] text-center font-bold absolute">
+            내가 좋아하는 <span className="bg-white px-4 mx-2 text-4xl rounded-lg">asdfasdf</span>이
+          </T1>
+          <T1 className="font-bold">모두 같은 그룹이라면?</T1>
+          <div className=" bg-slate-500 w-full h-[100px]">asdf</div>
+        </div>
+        <div className="bg-black absolute w-1 h-[100%] bottom-0" ref={setTargetSecondSection}></div>
+      </section>
+
+      {!isMainSeen && (
+        <Link href="/casting">
+          <Button isWhite={isSecondSectionSeen} isFixed={true} style={{ transition: "all 0.3s" }}>
+            <T2>걸그룹 만들러가기 🎀</T2>
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
